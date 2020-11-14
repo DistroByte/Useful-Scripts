00d5fcac269fede047c72dc06109bb789ca839af
JHackett <58866873+James-Hackett@users.noreply.github.com>
2020-11-14 15:47:58 +0000

    :sparkles: Feat: adds full logging to txt file, grep to console

--- dcu-scripts/checklabsheet.sh
+++ dcu-scripts/checklabsheet.sh
@@ -10,7 +10,7 @@ user=$(< ../username.txt)
 read labsheet
 
 while true; do
-  curl -s --head --user "$user:$password" https://ca116.computing.dcu.ie/labsheet-$labsheet.html head -n 1 | grep "HTTP" > "online.txt"
-  cat "online.txt"
+  curl -s --head --user "$user:$password" https://ca116.computing.dcu.ie/labsheet-$labsheet.html head -n 1 > "online.txt"
+  cat "online.txt" | grep "HTTP"
   sleep 300
 done
